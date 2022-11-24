da3708fb7552491ac117f54dba7f7ba175359487
Indrini <indra_barnes@hotmail.co.uk>
2022-11-24 14:39:23 +0000

    Adding button functionality

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 "use strict";
-const btn1 = document.querySelector(".btn1");
-const btn2 = document.querySelector(".btn2");
-const btn3 = document.querySelector(".btn3");
+const buttons = document.querySelectorAll(".btn");
+const image = document.querySelectorAll(".project-img");
+const fullDescription = document.querySelectorAll(".full-description");
+const description = document.querySelectorAll(".description");
+const closeButtons = document.querySelectorAll(".close-btn");
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    image[i].classList.toggle("hidden");
+    fullDescription[i].classList.toggle("hidden");
+    buttons[i].classList.toggle("hidden");
+    description[i].classList.toggle("hidden");
+    closeButtons[i].classList.toggle("hidden");
+  });
+}
+
+for (let i = 0; i < closeButtons.length; i++) {
+  closeButtons[i].addEventListener("click", function () {
+    image[i].classList.toggle("hidden");
+    fullDescription[i].classList.toggle("hidden");
+    buttons[i].classList.toggle("hidden");
+    description[i].classList.toggle("hidden");
+    closeButtons[i].classList.toggle("hidden");
+  });
+}
